bef9c4ae7bc1b1db0b00af9d8ac9cfc65f4900bd
kwtjdtn <kwtjdtn1@naver.com>
2020-09-29 20:04:13 +0900

    portfolio

---
+++ src/component/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="main-nav">
+      <ul className="nav-ul">
+        <li>Home</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
