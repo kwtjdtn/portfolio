b3de875ab3d47a0cd28bb22fb1a0cfc8c88dbbec
kwtjdtn <kwtjdtn1@naver.com>
2020-09-30 17:38:09 +0900

    project 내용 수정

--- src/component/Project.js
+++ src/component/Project.js
@@ -13,23 +13,23 @@ const Project = () => {
       <div className = "project-li">
           <div className = "project-con">
               <a href = "https://github.com/kwtjdtn/Team1"><img src = {timetable} alt ='timetable' className="project-img"/></a>
-              <div className = "project-text">공강시간 찾기 Project</div>
+              <div className = "project-text">공강시간 찾기 Project (HTML)</div>
               <div></div>
           </div>
           <div className = "project-space" />
           <div className = "project-con">
               <a href = "https://github.com/kwtjdtn/concat"><img src = {concat} alt='concat' className = "project-img"/></a>
-              <div className = "project-text">Concat Web Publishing</div>
+              <div className = "project-text">Concat Web Publishing (React.js)</div>
           </div>
           <div className = "project-space" />
           <div className = "project-con">
               <a href = "https://github.com/kookmin-sw/capstone-2020-4"><img src = {capstone} alt='capstone' className= "project-img"/></a>
-              <div className = "project-text">Capstone project</div>
+              <div className = "project-text">Capstone project (React.js) </div>
           </div>
           <div className = "project-space" />
           <div className = "project-con">
               <a href = "#"><img src={IKIK} alt ='IKIK' className = "project-img"/></a>
-              <div className = "project-text">IKIK project (진행중)</div>
+              <div className = "project-text">IKIK project (R/N 진행중)</div>
           </div>
       </div>
     </div>
