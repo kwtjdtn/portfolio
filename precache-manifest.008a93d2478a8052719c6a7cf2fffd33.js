a4b0b3927e150d282782cbac363efe99afc19487
kwtjdtn <kwtjdtn1@naver.com>
2020-10-07 15:25:08 +0900

    Updates

---
+++ precache-manifest.008a93d2478a8052719c6a7cf2fffd33.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf1c10fedc7b4ae39d92af570983eb3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85c433128741c52468e2",
+    "url": "/portfolio/static/css/main.6d6b4f34.chunk.css"
+  },
+  {
+    "revision": "b4c19a34eac8ad4d514a",
+    "url": "/portfolio/static/js/2.05d8a699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.05d8a699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c433128741c52468e2",
+    "url": "/portfolio/static/js/main.0a700355.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8f47d27d23d80a5776efd20e30bbecd5",
+    "url": "/portfolio/static/media/IKIK.8f47d27d.png"
+  },
+  {
+    "revision": "d7c4bb6cc69192432eea2124eac30fdd",
+    "url": "/portfolio/static/media/NanumSquareOTFBold.d7c4bb6c.otf"
+  },
+  {
+    "revision": "702510f5ef52748fed5abad03e0946d3",
+    "url": "/portfolio/static/media/NanumSquareOTFExtraBold.702510f5.otf"
+  },
+  {
+    "revision": "c40969eb31e6b4fde6545d9ceb518288",
+    "url": "/portfolio/static/media/NanumSquareOTFLight.c40969eb.otf"
+  },
+  {
+    "revision": "94caf6ad65d567bf536b3dfc12d1ae81",
+    "url": "/portfolio/static/media/NanumSquareOTFRegular.94caf6ad.otf"
+  },
+  {
+    "revision": "23edd0c87f28ed20504d3afc1a5977c5",
+    "url": "/portfolio/static/media/about.23edd0c8.png"
+  },
+  {
+    "revision": "324fa008307379fef1bf6e868169af54",
+    "url": "/portfolio/static/media/capstone.324fa008.png"
+  },
+  {
+    "revision": "c6ad36f513f64c6e617b7d206950a145",
+    "url": "/portfolio/static/media/concat.c6ad36f5.png"
+  },
+  {
+    "revision": "3acc202bcb31e48182c634f20126e410",
+    "url": "/portfolio/static/media/home-img.3acc202b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "84fb31343c4f2f52850e06d2f19ebd41",
+    "url": "/portfolio/static/media/timetable.84fb3134.png"
+  }
+]);
